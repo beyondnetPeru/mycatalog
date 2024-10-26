dc46714cae3133fddf7654a8e55724274cbf6815
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2024-10-26 08:53:10 -0500

    Initial Commit

---
+++ src/Services/MyPlanner.Catalog.Api/Companies/GetCompanies/GetCompaniesEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using MyCatalog.Shared.Models.Pagination;
+using MyPlanner.Catalog.Api.Models;
+
+namespace MyPlanner.Catalog.Api.Companies.GetCompanies
+{
+    public record GetCompanyResponse(IEnumerable<Company> Companies);
+
+    public class GetCompaniesEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/companies/", async ([FromBody] PaginationQuery paginationRequest, [AsParameters] CompanyServices services) =>
+            {
+                var query = paginationRequest.Adapt<GetCompaniesQuery>();
+
+                var response = await services.Mediator.Send(query);
+
+                return Results.Ok(response);
+            })
+                .WithTags(ENDPOINT.Tag)
+                .WithName(ENDPOINT.LIST.Name)
+                .Produces<IEnumerable<Company>>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary(ENDPOINT.LIST.Summary)
+                .WithDescription(ENDPOINT.LIST.Description);
+        }
+    }
+}
---
+++ src/Services/MyPlanner.Catalog.Api/Models/Company.cs
@@ -0,0 +1,12 @@
+﻿namespace MyPlanner.Catalog.Api.Models
+{
+    public class Company
+    {
+        public string Id { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public Company()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
