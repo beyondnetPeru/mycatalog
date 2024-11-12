b4e9cb1e11fe224a322faa3d7117489cf9d99cee
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2024-11-12 18:02:39 -0500

    refactoring

--- src/Clients/MyPlanner.Catalog.SpaWebClient/Components/Models/Product.cs
+++ src/Clients/MyPlanner.Catalog.SpaWebClient/Components/Models/Product.cs
@@ -1,9 +1,12 @@
-﻿namespace MyPlanner.Catalog.SpaWebClient.Components.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPlanner.Catalog.SpaWebClient.Components.Models
 {
     public class Product
     {
         public string Id { get; set; } = default!;
         public string TenantId { get; set; } = default!;
+        [Required]
         public string Name { get; set; } = default!;
         public List<string> Category { get; set; } = new();
         public string Description { get; set; } = default!;
